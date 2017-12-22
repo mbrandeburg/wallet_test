f73af9091de38b0614abc45d0b973191ec580aef
mbrandeburg <matthewbrandeburg@gmail.com>
2017-12-22 13:27:47 -0500

    got submit to hit the db, but need to build a show action for the redirect on the UI end

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,4 +4,24 @@ class AccountsController < ApplicationController
     # note that after you add this, it still won't show anything
     # need to go back to new.html.erb and fill out the form part so it shows stuff
   end
+  
+  def create
+#     render plain: params[:account].inspect
+    # ^^ allows us to see that when you hit submit, shit is getting passed through appropriately
+    # NOW we need to save that - need to whitelist it first, so make private below
+    @account = Account.new(account_params)
+    @account.save
+    
+    # after saving, we need to show something to the user, so let's redirect!
+    # do rake routes and see that accounts#show is the right path for:
+    redirect_to accounts_show(@account)
+    # but we still need to build that show action first...whoops
+  end
+  
+  private
+    def account_params
+      params.require(:account).permit(:title, :description)
+      #require top level key, then "permit" the params we want - only title and description!
+      # only this shit will then be saved per the create action above
+    end 
 end
