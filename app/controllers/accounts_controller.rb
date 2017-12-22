31d9126c6f61efa292c998d9ce5ac0ee35e690a4
mbrandeburg <matthewbrandeburg@gmail.com>
2017-12-22 15:54:10 -0500

    Got the new user account functionality with show, need back buttons and authenticator

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -8,21 +8,28 @@ class AccountsController < ApplicationController
   def create
 #     render plain: params[:account].inspect
     # ^^ allows us to see that when you hit submit, shit is getting passed through appropriately
+    
     # NOW we need to save that - need to whitelist it first, so make private below
     #### OOOOH AND B/C OF VALIDATION MAKE AN IF CLAUSE AROUND ALL THIS SHIT
-    
+    @account = Account.new(account_params)
     if @account.save
-#     @account = Account.new(account_params)
-#     @account.save
       # after saving, we need to show something to the user, so let's redirect!
       # do rake routes and see that accounts#show is the right path for:
       flash[:notice] = "Account was successfully created"
+      # before doing the next part, just realied, need to work with flash notice, so check out application's erb! (under views -> layout)
       redirect_to account_path(@account)
       # but we still need to build that show action first...whoops
     else
       render :new
     end
    
+  end
+
+  # show controller time
+  def show
+    #notice that the url for the show error is account/id# so...
+    @account = Account.find(params[:id])
+    # now make your views -> accounts -> show.html.erb
   end
   
   private
